0a8b06877639155f41f7fe0b75abce09540d7472
Danson <dansonkalaghe@gmail.com>
2026-06-06 18:13:55 +0300

    chromadb test embeddings

---
+++ vector_db/vector_client.py
@@ -0,0 +1,27 @@
+import chromadb
+
+client = chromadb.Client()
+
+collection = client.create_collection("news_atricles")
+
+collection.add(
+    ids=["id1", "id2", "id3", "id4"],
+    documents=[
+        "Apple is leading in a smart phone game with iPhone sales up by 35%",
+        "Tesla booked a minor profit of 1 billion $ in Q2",
+        "Apples are high in fiber, vitamin C, and various antioxidants",
+        "SpaceX got NASA contract worth 10 billion $",
+    ]
+)
+
+
+data = collection.get(
+    include = ["documents", "metadatas", "embeddings"]
+)
+
+
+for i, emb in enumerate(data["embeddings"]):
+    print(f"\nDocument {i +1}")
+    print("Text:", data["documents"][i])
+    print("Embedding length:", len(emb))
+    print("First 10 values of the embedding:", emb[:10])
